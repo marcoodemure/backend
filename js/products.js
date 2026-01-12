348fd28a2b3f5cf705f993eea3186ddd606ba328
marcoodemure <fedillagamarkludgelii@nddu.edu.ph>
2026-01-12 22:26:50 +0800

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,5 @@
+export const products = [
+  { id: "lamp", name: "Vintage Lamp", price: 20 },
+  { id: "chair", name: "Wooden Chair", price: 15 },
+  { id: "radio", name: "Retro Radio", price: 30 }
+];
