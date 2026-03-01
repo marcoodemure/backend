3fcedcfb5fde71e8c5cee09fa6ef6ec3157b0ef6
Administrator <admin@backend.local>
2026-03-01 22:01:01 +0800

    chore: update firebase-config formatting

--- backendprod/Website/firebase-config.js
+++ backendprod/Website/firebase-config.js
@@ -1,14 +1,14 @@
 (function () {
   const firestoreDatabaseId = "";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDTSdGQo-CVxSyig4V1oIz60WDZmy62HuQ",
-  authDomain: "backend-153ff.firebaseapp.com",
-  projectId: "backend-153ff",
-  storageBucket: "backend-153ff.firebasestorage.app",
-  messagingSenderId: "186787272282",
-  appId: "1:186787272282:web:9e59cda437ce90da90ea63"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDTSdGQo-CVxSyig4V1oIz60WDZmy62HuQ",
+    authDomain: "backend-153ff.firebaseapp.com",
+    projectId: "backend-153ff",
+    storageBucket: "backend-153ff.firebasestorage.app",
+    messagingSenderId: "186787272282",
+    appId: "1:186787272282:web:9e59cda437ce90da90ea63"
+  };
 
   const configured = Object.values(firebaseConfig).every(
     (value) => typeof value === "string" && value.length > 0 && !value.startsWith("REPLACE_WITH_")
